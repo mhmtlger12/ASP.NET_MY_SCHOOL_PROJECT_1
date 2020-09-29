6bf02ce2943f8e3c4aabcfdd409060c2fcefb6d7
MEHMET <64442588+mhmtlger12@users.noreply.github.com>
2020-09-29 22:16:32 +0300

    İlk yüklemem

---
+++ Proje1_OkulKayit/KayitOl.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proje1_OkulKayit
+{
+    public partial class KayitOl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLOGRENCITableAdapter dt = new DataSet1TableAdapters.TBLOGRENCITableAdapter();
+            dt.Ogrencı_Ekle(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text);
+        }
+    }
+}
